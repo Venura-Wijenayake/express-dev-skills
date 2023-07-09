dcb64370801001c3210898d17ed5eab82a0e778d
Venura-Wijenayake <venu.wije@gmail.com>
2023-07-08 22:46:49 -0700

    Dev skills Lab - Skills list complete (Future update: could use some more content).

--- models/skill.js
+++ models/skill.js
@@ -1,9 +1,18 @@
 // models/skill.js
 
 const skills = [
-    {id: 125223, skill: 'Feed Dogs', done: true},
-    {id: 127904, skill: 'Learn Express', done: false},
-    {id: 139608, skill: 'Buy Milk', done: false}
+    {id: 100001, skill: 'C++/C#', done: true},
+    {id: 100002, skill: 'Java', done: true},
+    {id: 100003, skill: 'HTML', done: true},
+    {id: 100004, skill: 'CSS', done: true},
+    {id: 100005, skill: 'Javascript', done: true},
+    {id: 100006, skill: 'Node.js', done: false},
+    {id: 100007, skill: 'Express', done: false},
+    {id: 100008, skill: 'MongoDB', done: false},
+    {id: 100009, skill: 'Python', done: false},
+    {id: 100010, skill: 'Django', done: false},
+    {id: 100011, skill: 'PostgreSQL', done: false},
+    {id: 100012, skill: 'React', done: false},
   ];
   
   module.exports = {
