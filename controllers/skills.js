ea2fbd85e4a9f93a154ea70f98906804e9ae9e29
Venura-Wijenayake <venu.wije@gmail.com>
2023-07-08 21:32:00 -0700

    Dev skills 1 - copying express todos over

---
+++ controllers/skills.js
@@ -0,0 +1,22 @@
+// controllers/todos.js
+
+ // Convention is to name the model in uppercase and singular
+ const Todo = require('../models/todo');
+
+module.exports = {
+    index,
+    show,
+};
+
+function index(req, res) {
+    res.render('todos/index', {
+      todos: Todo.getAll()
+    });
+  }
+
+function show(req, res) {
+    res.render('todos/show', {
+        todo: Todo.getOne(req.params.id),
+    });
+}
+  
